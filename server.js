e85c819be9d97b787846b1a153f0ad5d9b832072
A M Isaac <aisaac@wccnet.edu>
2020-07-09 14:29:07 -0400

    test IF statement

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var PORT = process.env.PORT || 5000 ;
 var http = require('http');
 http.createServer(function (request, response) {
     //console.log('request ', request.url);
+	response.write('('+ request.url + ')');
 	if(request.url=='abc'){
 		response.write('You sent a request from abc');
 	}
