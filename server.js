665eae100a133941a0fdf1142d2de4391c9845fc
A M Isaac <aisaac@wccnet.edu>
2020-07-09 16:29:11 -0400

    JM proving a point -2

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ http.createServer(function (request, response) {
     //console.log('request ', request.url);
 	if(request.url=='/home'){
 		response.write('('+ request.url + ')');
-		fs.readFile('./home.htm', function(error, content) {
+		fs.readFile('./home.htm', function(error, content) { //fs.readFile is an asynchronous method.JavaScript is asynchronous single thread - look up JavaScript execution model
 			if (error) {
 				response.writeHead(404, { 'Content-Type': 'text/html' });
 				response.end('Bad stuff happened! Error code: ' + error.code + '\n' );
 			}
 			else {
+				response.write('In the else');
 				response.writeHead(200, { 'Content-Type': 'text/html' });
 				response.end(content, 'utf-8');
 			}
@@ -19,9 +20,11 @@ http.createServer(function (request, response) {
 	}
 	else if(request.url == '/manic'){
 		response.write('You are manic.  Take a chill pill!');
+		response.end('Ending manic');
 	}
 	else{
 		response.write('No Go');
+		response.end('ending No Go');
 	}
-    response.end("[HEROKU_NODEJS_MINIMAL]");
+    //response.end("[HEROKU_NODEJS_MINIMAL]");
 }).listen(PORT);
