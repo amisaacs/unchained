e0f8830ba018bc6d75146114d4759cd0b81d18c8
A M Isaac <aisaac@wccnet.edu>
2020-07-09 16:04:51 -0400

    add serve html file to server.js

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ http.createServer(function (request, response) {
     //console.log('request ', request.url);
 	response.write('('+ request.url + ')');
 	if(request.url=='/home'){
-		response.write('You sent a request from home');
+		fs.readFile('./home.htm', function(error, content) {
+			if (error) {
+				response.writeHead(404, { 'Content-Type': 'text/html' });
+				response.end('Bad stuff happened! Error code: ' + error.code + '\n' );
+			}
+			else {
+				response.writeHead(200, { 'Content-Type': 'text/html' });
+				response.end(content, 'utf-8');
+			}
+		});
 	}
 	else if(request.url == '/manic'){
 		response.write('You are manic.  Take a chill pill!');
