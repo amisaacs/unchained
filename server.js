b6bc26c4e8a79d725dcc77f8bbd7c66342f9eba5
A M Isaac <aisaac@wccnet.edu>
2020-07-09 14:18:59 -0400

    test url routing

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var PORT = process.env.PORT || 5000 ;
 var http = require('http');
 http.createServer(function (request, response) {
-    console.log('TESTrequest ', request.url);
+    //console.log('request ', request.url);
+	if(request.url=='abc'){
+		response.write('You sent a request from abc');
+	}
+	if(request.url == 'manic'){
+		response.write('You are manic.  Take a chill pill!');
+	}
     response.end("[HEROKU_NODEJS_MINIMAL]");
 }).listen(PORT);
