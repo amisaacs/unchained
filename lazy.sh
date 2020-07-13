e3a6b12b754b5cf15ac160036eaf78cb2d69c7c6
A M Isaac <aisaac@wccnet.edu>
2020-07-12 21:29:56 -0400

    auto commit for debugging

---
+++ lazy.sh
@@ -0,0 +1,3 @@
+ git add . && git commit -m 'auto commit for debugging' && git push heroku master
+
+read -p "press any key to continue"
--- server.js
+++ server.js
@@ -1,13 +1,39 @@
+/*
+Example deployment can be seen at:
+https://un-chained.herokuapp.com/home
+
+Deployment Instructions (Recreating the steps JM taught me):
+1. Clone https://github.com/amisaacs/unchained repository.
+2. Remove .git folder in the folder where the project was cloned to.
+3. Download heroku CLI.
+4. Create heroku account.
+5. Open GitBash in folder where the project was cloned to.
+	a. Login to heroku account 
+		$ heroku login
+	b.	Create heroku app
+		$heroku apps:create appname
+			Will return a web url and a git url - save these.
+			$git remote add heroku https://git.heroku.com/appname.git
+			$git push heroku master
+    6. Browser[ https://appname.herokuapp.com/ ]
+	c. Initialize repository:
+		$ git init . 
+		$ git add . heroku
+		$ git commit -m 'set up repository'
+		$ git remote add 
+		$ git remote -v
+		$ git push remoteName master
+		Copy url to appName
+*/
+
+
 var PORT = process.env.PORT || 5000 ;
 var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (request, response) {
-    //console.log('request ', request.url);
 	if(request.url=='/home'){
-		//response.write('('+ request.url + ')');
-		fs.readFile('./home.htm', function(error, content) { //fs.readFile is an asynchronous method.JavaScript is asynchronous single thread - look up JavaScript execution model
-			// response.write(error.code);
+		fs.readFile('./home.htm', function(error, content) { 
 			if (error) {
 				//response.writeHead(404, { 'Content-Type': 'text/html' });
 				response.end('Bad stuff happened!');
@@ -16,7 +42,6 @@ http.createServer(function (request, response) {
 				response.writeHead(200, { 'Content-Type': 'text/html' });
 				response.end(content, 'utf-8');
 			} 
-			response.end('testing');
 		});
 	}
 	else if(request.url == '/manic'){
